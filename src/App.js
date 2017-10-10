d6a5bd6d48eccf653109361c1648a750372e55cc
Antikarat <antonliashok@gmail.com>
2017-10-10 17:01:47 +0300

    corect work with reply

--- src/App.js
+++ src/App.js
@@ -14,17 +14,18 @@ export default class App extends Component {
     this.delBut = this.delBut.bind(this);
     this.repBut = this.repBut.bind(this);
     this.LoadMoreHandler = this.LoadMoreHandler.bind(this);
-    this.repInput = this.repInput.bind(this);
+    this.addNewReplyButton = this.addNewReplyButton.bind(this);
+    //for new reply comment
+    this.replyInput = this.replyInput.bind(this);
     this.state = {
       currentUser: 'Antony Musk',
       comments: [
-        { user: 'Kurt Thompson',
+        { user: 'Miranda Thompson',
           photo: avatar, 
           coment: 'If non everyone makes money blogging, why is blogging so popular?', 
           time: '2015-07-06 at 13-57',
           sw: true,
-          replyValue: [  ],
-          showRep: false
+          replyValue: [ ],
         }
       ],
       newComment: { 
@@ -33,14 +34,22 @@ export default class App extends Component {
         coment: '', 
         time: 0, 
         sw: true ,
-        replyValue: '',
-        showRep: false
+        replyValue: [ ],
       },
-      newReply: {},
+      newReply: { user: 'Antony Musk', photo: avatar, coment: '', time: 0 },
       limit: 5
     }
   }
 
+  componentDidMount() {
+    //   $('.replyTarget').click( () => {
+    //     //console.log('!');
+    //     $(this).parent('.commentWrapper').find('.addReplyArea').fadeOut('slow');
+    // });
+    $('.replyTarget').click( function() {
+      $(this).parent('.commentWrapper').css({'background':'red'});
+    })
+  }
   handlerNEWComment(e) {
       this.state.newComment['coment'] = e.target.value;
       this.setState({newComment: this.state.newComment});
@@ -54,7 +63,8 @@ export default class App extends Component {
     //push new comment in main arr
     this.state.comments.push(this.state.newComment);
     this.setState({comments: this.state.comments,
-      newComment: { user: 'Kurt Thompson', photo: avatar, coment: '', time: 0, sw: true }});
+      newComment: { user: 'Kurt Thompson', photo: avatar, coment: '', time: 0, sw: true,
+      replyValue: [ { user: 'Antony Musk', photo: avatar, coment: 'example comment', time: 0 } ] }});
   }
 
   /*buttons: edit, del, reply*/
@@ -79,12 +89,18 @@ export default class App extends Component {
     this.setState({limit: this.state.limit + 5});
     console.log(this.state.limit);
   }
-  repInput(id, e) {
-    this.state.comments[id]['replyValue'] = e.target.value;
-    this.setState({replyValue: this.state.comments});
+
+  //for new reply comment
+  replyInput(e) {
+    this.state.newReply['coment'] = e.target.value;
+    this.setState({coment: this.state.newReply});
+  }
+  addNewReplyButton(id) {
+    this.state.comments[id].replyValue.push(this.state.newReply);
+    this.setState({ replyValue: this.state.comments, newReply: { user: 'Antony Musk', photo: avatar, coment: '', time: 0 } });
   }
   render() {
-    console.log(this.state.comments[0]);
+    //console.log(this.state.newReply.coment);
     let countCommets = this.state.comments.length;
     const listOFcomments = this.state.comments.slice(0, this.state.limit).map( (item, id) => {  //
       return <Comments
@@ -99,10 +115,13 @@ export default class App extends Component {
                   delBut={this.delBut}
                   repBut={this.repBut}
                   sw={item.sw}
-                  showRep={item.showRep}
                   curUser={this.state.currentUser}
-                  repInput={this.repInput}
+                  
                   replyValue={item.replyValue}
+                  //for new reply comment
+                  newReply={this.state.newReply}
+                  replyInput={this.replyInput}
+                  addNewReplyButton={this.addNewReplyButton}
                   
       />
     })
@@ -153,10 +172,13 @@ class Comments extends Component {
           editInp, 
           delBut, 
           repBut,
-          showRep,
           curUser,
-          repInput,
-          replyValue } = this.props;
+          replyValue,
+          //for new reply comment
+          newReply,
+          replyInput,
+          addNewReplyButton } = this.props;
+          
     return <div className="commentWrapper">
             <img src={ava} alt={name}/>
             <div>
@@ -170,63 +192,58 @@ class Comments extends Component {
               </p>
               <button onClick={editBut.bind(null, index)}>{sw ? `Edit`:`Ok`}</button>
               <button onClick={delBut.bind(null, index)}>Delete</button>
-              <button onClick={repBut.bind(null, index)}>Reply</button>
-              { !showRep ? null : 
-                <div className="blockFOReplay">
-                    <img src={ava} alt={name}/>
-                    <div>
-                      <p>{curUser} to {name} <span> {time}</span></p>
-                      <TextAreaReply
-                        repInput={repInput}
-                        replyValue={replyValue}
-                        index={index}
-                      />
-                    </div>
-                </div>}
+              <button className="replyTarget" onClick={repBut.bind(null, index)}>Reply</button>
+
+              <ReplyComments
+                index={index}
+                name={name}
+                replyValue={replyValue}
+              />
+              <AddReplyComment
+                index={index}
+                newReply={newReply}
+                replyInput={replyInput}
+                addNewReplyButton={addNewReplyButton}
+              />
+
             </div>
+  
     </div>
   }
 }
 
-class TextAreaReply extends Comments {
-  constructor(props) {
-    super();
-    this.state = {
-      saveReply: false
-    }
-  }
-  saveRepFunc() {
-    this.setState({saveReply: !this.state.saveReply});
-  }
+class ReplyComments extends Comments {
   render() {
-    return <div>
-        {
-          !this.state.saveReply 
-          ?
-          <textarea
-            type="text"
-            value={this.props.replyValue}
-            onChange={this.props.repInput.bind(null, this.props.index)}
-            onBlur={this.saveRepFunc.bind(this)} /> 
-          :
-          <p onClick={this.saveRepFunc.bind(this)}>{this.props.replyValue}</p>
-        }
-    </div>
+    let { name, replyValue } = this.props;
+    let lostOfReplyes = this.props.replyValue.map( (i, id) => {
+      return  <li key={id}>
+                    <img src={i.photo}/>
+                    <ul>
+                        <li><span>{i.user}</span> to <span>{name}</span> <span>{i.time}</span></li>
+                        <li>{i.coment}</li>
+                    </ul>
+              </li>
+    })
+    return <ul className="replysComponentBlock">
+      {lostOfReplyes}
+    </ul>
   }
 }
 
 
+class AddReplyComment extends Comments {
+  render() {
+    let { index, addNewReplyButton } = this.props;
+    return <div className="addReplyArea">
+            <textarea
+                type="text"
+                value={this.props.newReply.coment}
+                onChange={this.props.replyInput.bind(null)}
+    />
+            <button data-cl={index} onClick={addNewReplyButton.bind(null,index)}>Add</button>
+    </div>
+  }
+}
 
 const LoadMore = (props) => <button onClick={props.more}>More</button>
 
-// {
-//   !this.state.saveReply 
-//   ?
-//   <textarea
-//     type="text"
-//     value={replyValue}
-//     onChange={repInput.bind(null, index)}
-//     onMouseLeave={this.blurHandler.bind(this)} /> 
-//   :
-//   <p>1</p>
-// }
